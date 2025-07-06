d069231a172fd5c2ffbebb5442cef4c260c55ec6
Boogeymaster <dr.smuuurge@gmail.com>
2025-07-06 22:14:22 +0300

    RA-08 add CategoriesListAdapter

--- app/src/main/java/com/example/androidsptrintxml/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsptrintxml/CategoriesListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidsptrintxml.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
@@ -20,6 +21,7 @@ class CategoriesListFragment : Fragment() {
     ): View {
         _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         val view = binding.root
+        initRecycler()
         return view
     }
 
@@ -27,4 +29,12 @@ class CategoriesListFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun initRecycler() {
+        val stub = STUB
+        val categoriesListAdapter = CategoriesListAdapter(stub.getCategories())
+        val recyclerView: RecyclerView = binding.rvCategories
+        recyclerView.adapter = categoriesListAdapter
+    }
+
 }
