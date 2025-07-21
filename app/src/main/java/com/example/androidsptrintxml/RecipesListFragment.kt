2d53fbe7acd004837781675050263f6855a8d8a9
Boogeymaster <dr.smuuurge@gmail.com>
2025-07-21 19:20:11 +0300

    RA-11 midjourney(1-3)

--- app/src/main/java/com/example/androidsptrintxml/RecipesListFragment.kt
+++ app/src/main/java/com/example/androidsptrintxml/RecipesListFragment.kt
@@ -1,11 +1,14 @@
 package com.example.androidsptrintxml
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidsptrintxml.databinding.FragmentListRecipeBinding
+import java.io.InputStream
 
 class RecipesListFragment : Fragment() {
 
@@ -31,6 +34,17 @@ class RecipesListFragment : Fragment() {
         categoryId = requireArguments().getInt(ARG_CATEGORY_ID)
         categoryTitle = requireArguments().getString(ARG_CATEGORY_NAME)
         categoryUrl = requireArguments().getString(ARG_CATEGORY_IMAGE_URL)
+        with(binding){
+            try {
+                val inputStream: InputStream? =
+                    categoryUrl?.let { view.context?.assets?.open(it) }
+                val drawable = Drawable.createFromStream(inputStream, null)
+                ivRecipesHeader.setImageDrawable(drawable)
+            } catch (e: Exception) {
+                Log.e("error", "Error drawable in onBindViewHolder", e)
+            }
+            tvRecipesHeader.text = categoryTitle
+        }
     }
 
     override fun onDestroyView() {
